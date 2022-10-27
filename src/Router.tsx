ec0a2e36700c35a840699489b436cb86030af8cb
lilicrst <lisandra.crisilva37@gmail.com>
2022-10-27 12:25:18 -0300

    feat: history page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { History } from './pages/History'
+import { History } from './pages/histoty'
 import { Home } from './pages/home'
 
 export function Router() {
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -15,13 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-sugestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="projeto 1" />
+            <option value="projeto 2" />
+            <option value="projeto 3" />
+          </datalist>
 
           <label htmlFor="minutsAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutsAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>

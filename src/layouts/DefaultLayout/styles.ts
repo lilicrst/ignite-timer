61a176a9f5544fbcfa53bd76678454005da05498
lilicrst <lisandra.crisilva37@gmail.com>
2022-10-27 10:24:06 -0300

    fix: height container

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
-  height: calc(100vh - 10rem);
+  max-width: 70rem;
+  height: 46.25rem; /* calc(100vh - 10rem) */
   margin: 5rem auto;
   padding: 2.5rem;
 
